478465b73658de6ea7ba7cc7a4b765f4d52878ff
Victor Ramirez <vicdeveloper83@gmail.com>
2022-12-19 15:38:36 +0000

    projects added

--- src/components/ProjectsImageData.jsx
+++ src/components/ProjectsImageData.jsx
@@ -1,7 +1,7 @@
 import projectImg1 from '../images/project1.jpg';
 import dogs from '../images/dogs.png';
 import github from '../images/github.webp';
-
+import games from '../images/games.png';
 export const ProjectsImageData = [
 	{
 		imageName: dogs,
@@ -17,18 +17,19 @@ export const ProjectsImageData = [
 		subtitle: 'This project was elaborated using React.js',
 		pathname: 'https://github.com/bukaro8/dogs-deploy',
 	},
-	// {
-	// 	imageName: projectImg1,
-	// 	title: 'Project Name',
-	// 	subtitle: 'This project was elaborated using React.js',
-	// 	pathname: 'https://www.google.com',
-	// },
-	// {
-	// 	imageName: projectImg1,
-	// 	title: 'Project Name',
-	// 	subtitle: 'This project was elaborated using React.js',
-	// 	pathname: 'https://www.google.com',
-	// },
+	{
+		imageName: games,
+		title: 'VideoGames Library',
+		subtitle:
+			'This project was elaborated using React.js, Node.js/ Express, Sequelize and PostgreSQL',
+		pathname: 'https://front-videogamest.vercel.app/',
+	},
+	{
+		imageName: github,
+		title: 'VideoGames Library Repo',
+		subtitle: 'This project was elaborated using React.js',
+		pathname: 'https://github.com/bukaro8/videogames',
+	},
 	// {
 	// 	imageName: projectImg1,
 	// 	title: 'Project Name',
